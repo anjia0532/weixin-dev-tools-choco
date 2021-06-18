8c6d79c25debe46d489d8c3fb8b06148eecfc601
赵安家 <anjia0532@gmail.com>
2021-06-18 19:03:20 +0800

    'first'

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,52 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$softwareName = 'wechat-devtools*'
+[array]$key = Get-UninstallRegistryKey -SoftwareName $softwareName
+
+if ($key.Count -gt 0) {
+    Invoke-Expression -Command $PSScriptRoot\chocolateyUninstall.ps1
+}
+
+$url = 'https://servicewechat.com/wxa-dev-logic/download_redirect?type=ia32&from=mpwiki&download_version=1052105170&version_type=1'
+$checksum = '95578F85C9760A763D78D9F935ED17994F0B2EE944EAEA42810586F39EB78A0A'
+
+$url64 = 'https://servicewechat.com/wxa-dev-logic/download_redirect?type=x64&from=mpwiki&download_version=1052105170&version_type=1'
+$checksum64 = 'F4862E399D74EB0888115DC90E9724380DD266E5F896653BF86FEED4D5987C97'
+
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+if ([System.Environment]::Is64BitOperatingSystem) {
+    $programFiles = $env:ProgramFiles
+}
+else {
+    $programFiles = ${env:ProgramFiles(x86)}
+}
+$installDir = "$programFiles\win-rdm"
+
+$pp = Get-PackageParameters
+if ($pp.InstallDir) {
+    $installDir = $pp.InstallDir
+}
+
+$silentArgs = "/S /D=$installDir"
+
+New-Item -ItemType Directory -Force -Path $installDir
+
+$packageArgs = @{
+    packageName    = $env:ChocolateyPackageName
+    unzipLocation  = $toolsDir
+    fileType       = 'exe'
+    url            = $url
+    url64bit       = $url64
+
+    softwareName   = $softwareName
+
+    checksum       = $checksum
+    checksumType   = 'sha256'
+    checksum64     = $checksum64
+    checksumType64 = 'sha256'
+
+    silentArgs     = $silentArgs
+    validExitCodes = @(0, 1641, 3010)
+}
+
+Install-ChocolateyPackage @packageArgs
